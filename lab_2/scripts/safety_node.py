ec60bfea03116749677e8ea59ea33ec52f55a0e4
Ze Jie <ze-jie-kong-@hotmail.com>
2022-03-13 12:10:51 +0800

    /brake not receiving messages ...

--- lab_2/scripts/safety_node.py
+++ lab_2/scripts/safety_node.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 import rospy
+from std_msgs.msg import Header,Bool
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from ackermann_msgs.msg import AckermannDriveStamped
+import threading
+import math
 
 # TODO: import ROS msg types and libraries
 
@@ -21,21 +27,69 @@ class Safety(object):
         NOTE that the x component of the linear velocity in odom is the speed
         """
         self.speed = 0
+        self.ttc_threshold = 0 
         # TODO: create ROS subscribers and publishers.
+        self.scan_subscriber = threading.Thread(target=self.sub_scan)
+        self.odom_subscriber = threading.Thread(target=self.sub_odom)
+        self.brake_publisher = None
+        self.brake_bool_publisher = None
 
     def odom_callback(self, odom_msg):
         # TODO: update current speed
-        self.speed = 0
+        self.speed = odom_msg.twist.twist.linear.x
 
     def scan_callback(self, scan_msg):
         # TODO: calculate TTC
+        angle = 0
+        min_ttc = math.inf
+        for i in scan_msg.ranges:
+            v_i = self.speed * math.cos(angle)
+            if v_i > 0:
+                ttc = i / v_i
+            else:
+                ttc = math.inf
+            
+            if ttc < min_ttc:
+                min_ttc = ttc
+        
+            angle += scan_msg.angle_increment
 
         # TODO: publish brake message and publish controller bool
+        brake_bool_msg = Bool()
+        if min_ttc < self.ttc_threshold:
+            brake_msg = AckermannDriveStamped()
+            header = Header()
+            header.stamp = rospy.Time.now()
+            brake_msg.header = header
+            brake_msg.drive.steering_angle = 0
+            brake_msg.drive.steering_angle_velocity = 0
+            brake_msg.drive.speed = 0
+            brake_msg.drive.acceleration = 0
+            brake_msg.drive.jerk = 0
+            brake_bool_msg.data = True
+            self.brake_publisher.publish(brake_msg)
+        else:
+            brake_bool_msg.data = False
+        self.brake_bool_publisher.publish(brake_bool_msg)
+
+    def sub_scan(self):
+        sub_scan = rospy.Subscriber("scan",LaserScan,self.scan_callback)
+        rospy.spin()
+    
+    def sub_odom(self):
+        sub_odom = rospy.Subscriber("odom",Odometry,self.odom_callback)
+        rospy.spin()
+
+    def run(self):
+        rospy.init_node('safety_node')
+        self.ttc_threshold = rospy.get_param("ttc_threshold")
+        self.brake_publisher = rospy.Publisher("brake",AckermannDriveStamped,queue_size=0)
+        self.brake_bool_publisher = rospy.Publisher("brake_bool",Bool,queue_size=0)
+
+        self.scan_subscriber.start()
+        self.odom_subscriber.start()
 
 
-def main():
-    rospy.init_node('safety_node')
-    sn = Safety()
-    rospy.spin()
 if __name__ == '__main__':
-    main()
+    sn = Safety()
+    sn.run()
